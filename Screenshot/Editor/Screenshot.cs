a6d86ff9cfeba5aaf8e494df9dcc9ca5ca4d8c85
YASUI Kentarow <yasuikentarow@gmail.com>
2020-10-17 17:28:26 +0900

    キャプチャのファイルが作成されるまで待つDoCaptureを追加

--- Screenshot/Editor/Screenshot.cs
+++ Screenshot/Editor/Screenshot.cs
@@ -1,8 +1,9 @@
-using System;
+using System.Collections;
 using System.IO;
+using System;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
 
 namespace unitwb.editor {
 /**
@@ -17,17 +18,27 @@ public class Screenshot {
 
   /**
    */
-  public static void Capture(string postfix) {
+  public static string Capture(string postfix) {
     var assetDir = new DirectoryInfo(Application.dataPath);
     var dir = assetDir.Parent.CreateSubdirectory("Screenshots");
     var name = Application.productName + "-" + 
+      DateTime.Now.ToString("yyyyMMdd_HHmmss") + "-" + 
       SceneManager.GetActiveScene().name + "-" + 
-      Screen.width + "x" + Screen.height + "-" + 
-      DateTime.Now.ToString("yyyyMMdd_HHmmss") + 
+      Screen.width + "x" + Screen.height +
       postfix + ".png";
     var path = Path.Combine(dir.FullName, name);
     ScreenCapture.CaptureScreenshot(path);
     Debug.Log($"Capture {path}");
+    return path;
+  }
+
+  /**
+   */
+  public static IEnumerator DoCapture(string postfix) {
+    var path = Capture(postfix);
+    while(!File.Exists(path)) {
+      yield return null;
+    }
   }
 }
 }
