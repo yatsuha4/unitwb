106ee9b1e5a02dac5c4ec2493fbea047bee56637
YASUI Kentarow <yasuikentarow@gmail.com>
2020-12-12 10:41:34 +0900

    複数曲の切り替えに対応

--- Audio/Script/MusicPlayer.cs
+++ Audio/Script/MusicPlayer.cs
@@ -6,16 +6,38 @@ namespace unitwb {
 public class MusicPlayer
   : MonoBehaviour
 {
-  public AudioClip musicClip;
+  public AudioClip[] musicClips;
+  public float crossFadeTime = 1.0f;
 
   /**
    */
   void Start() {
+    if(this.musicClips.Length > 0) {
+      PlayMusic(this.musicClips[0]);
+    }
+  }
+
+  /**
+   */
+  public AudioObject PlayMusic(int index) {
+    return PlayMusic(this.musicClips[index]);
+  }
+
+  /**
+   */
+  public AudioObject PlayMusic(AudioClip musicClip) {
+    return PlayMusic(musicClip, this.crossFadeTime);
+  }
+
+  /**
+   */
+  public AudioObject PlayMusic(AudioClip musicClip, float crossFadeTime) {
     if(AudioManager.instance is AudioManager manager) {
       if(!manager.IsPlayMusic(musicClip)) {
-        manager.PlayMusic(musicClip);
+        return manager.PlayMusic(musicClip, crossFadeTime = crossFadeTime);
       }
     }
+    return null;
   }
 }
 }
