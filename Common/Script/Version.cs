14f1f1a071020126310965f545b5ce35892cc28f
YASUI Kentarow <yasuikentarow@gmail.com>
2021-09-09 16:52:47 +0900

    IComparable対応

--- Common/Script/Version.cs
+++ Common/Script/Version.cs
@@ -1,18 +1,71 @@
-namespace unitwb {
-/**
-   簡易バージョンクラス
-*/
-[System.Serializable]
-public class Version {
-  public int major;
-  public int minor;
+using System;
 
+namespace unitwb {
   /**
-     コンストラクタ
+     <summary>簡易バージョンクラス</summary>
   */
-  public Version(int major = 0, int minor = 0) {
-    this.major = major;
-    this.minor = minor;
+  [Serializable]
+  public struct Version
+    : IComparable<Version>
+  {
+    public int major;
+    public int minor;
+
+    /**
+       <summary>コンストラクタ</summary>
+    */
+    public Version(int major = 0, int minor = 0)
+    {
+      this.major = major;
+      this.minor = minor;
+    }
+
+    /**
+       <summary>比較</summary>
+     */
+    public int CompareTo(Version rhs)
+    {
+      int diff = this.major - rhs.major;
+      if(diff == 0)
+      {
+        diff = this.minor - rhs.minor;
+      }
+      return diff;
+    }
+
+    public static bool operator<(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) < 0;
+    }
+
+    public static bool operator<=(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) <= 0;
+    }
+
+    public static bool operator>(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) > 0;
+    }
+
+    public static bool operator>=(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) >= 0;
+    }
+
+    public static bool operator==(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) == 0;
+    }
+
+    public static bool operator!=(Version lhs, Version rhs)
+    {
+      return lhs.CompareTo(rhs) != 0;
+    }
+
+    override public string? ToString()
+    {
+      return $"{this.major}.{this.minor}";
+    }
   }
-}
 }
