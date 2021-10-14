7fa5d881d407bc29fc744699d1506778b3f96c2c
YASUI Kentarow <yasuikentarow@gmail.com>
2021-10-14 17:54:50 +0900

    namespace変更

--- Ranking/Ranking.cs
+++ Ranking/Ranking.cs
@@ -2,7 +2,8 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-namespace unitwb {
+namespace Towerb.Ranking
+{
   /**
      <summary>ランキング</summary>
   */
@@ -20,7 +21,8 @@ namespace unitwb {
     /**
        <summary>コンストラクタ</summary>
     */
-    public Ranking(Item[] items, Item self) {
+    public Ranking(Item[] items, Item self)
+    {
       this.items = items;
       this.self = self;
     }
@@ -36,26 +38,31 @@ namespace unitwb {
     public List<(int, Item)> GetRanking<Key>(Func<Item, bool> filter, 
                                              Func<Item, Key> order, 
                                              int max = 10, 
-                                             int near = 2) {
+                                             int near = 2)
+    {
       var ranking = new List<(int, Item)>();
       var items = this.items.
         Where(item => filter(item)).
         OrderBy(item => order(item)).
         ThenBy(item => ((item == this.self) ? 0 : 1)).
         ToArray();
-      if(this.self != null) {
+      if(this.self != null)
+      {
         var rank = Array.IndexOf(items, this.self);
-        if(rank >= max) {
+        if(rank >= max)
+        {
           for(int i = rank - near, 
                 bottom = Math.Min(rank + near + 1, items.Length);
-              i < bottom; i++) {
+              i < bottom; i++)
+          {
             ranking.Add((i, items[i]));
           }
         }
       }
       for(int i = 0, 
             bottom = Math.Min(max, items.Length) - ranking.Count;
-          i < bottom; i++) {
+          i < bottom; i++)
+      {
         ranking.Insert(i, (i, items[i]));
       }
       return ranking;
