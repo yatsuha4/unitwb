ad83eee917b8744a29830e3a412f1c2dacc7094d
YASUI Kentarow <yasuikentarow@gmail.com>
2021-10-14 09:56:47 +0900

    インジケーターをアニメーションで

--- Indicator/Script/Indicator.cs
+++ Indicator/Script/Indicator.cs
@@ -1,24 +1,18 @@
-using System.Collections;
 using UnityEngine;
 
-namespace unitwb.indicator {
+namespace Towerb.Indicator
+{
   /**
      インジケーター
   */
   public class Indicator
-    : MonoBehaviour
+    : SingletonBehaviour<Indicator>
   {
-    public GameObject Image;
-    public float Time = 0.1f;
-    public int Step = 9;
-
     /**
      */
-    IEnumerator Start() {
-      for(;;) {
-        yield return new WaitForSeconds(this.Time);
-        this.Image.transform.eulerAngles -= new Vector3(0.0f, 0.0f, 360.0f / this.Step);
-      }
+    public void Show(bool show)
+    {
+      GetComponent<Animator>().SetBool("Show", show);
     }
   }
 }
