247591c85c8d2c827590540a3f28b59d2a7f82d2
Yasui Kentarow <yasuikentarow@gmail.com>
2026-09-11 06:11:09 +0900

    style: 書式変更

--- SafeArea/Script/SafeArea.cs
+++ SafeArea/Script/SafeArea.cs
@@ -1,41 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace unitwb {
-/**
-   セーフエリア
-*/
-public class SafeArea
-  : MonoBehaviour
+namespace unitwb
 {
-  private RectTransform canvas;
-  private Rect rect;
+    /**
+     * <summary>セーフエリア</summary>
+     */
+    public class SafeArea: MonoBehaviour
+    {
+        private RectTransform canvas;
+        private Rect rect;
 
-  /**
-   */
-  void Awake() {
-    this.canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
-    UpdateLayout();
-  }
+        /**
+         */
+        void Awake()
+        {
+            this.canvas = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+            UpdateLayout();
+        }
 
-  /**
-   */
-  void Update() {
-    if(this.canvas.rect != this.rect) {
-      this.rect = this.canvas.rect;
-      UpdateLayout();
+        /**
+         */
+        void Update()
+        {
+            if(this.canvas.rect != this.rect)
+            {
+                this.rect = this.canvas.rect;
+                UpdateLayout();
+            }
+        }
+
+        /**
+         */
+        private void UpdateLayout()
+        {
+            var resolution = new Vector2(Screen.width, Screen.height);
+            var safeArea = Screen.safeArea;
+            var scale = (Vector2)this.canvas.localScale;
+            var transform = GetComponent<RectTransform>();
+            transform.offsetMin = safeArea.min / scale;
+            transform.offsetMax = (safeArea.max - resolution) / scale;
+        }
     }
-  }
-
-  /**
-   */
-  private void UpdateLayout() {
-    var resolution = new Vector2(Screen.width, Screen.height);
-    var safeArea = Screen.safeArea;
-    var scale = (Vector2)this.canvas.localScale;
-    var transform = GetComponent<RectTransform>();
-    transform.offsetMin = safeArea.min / scale;
-    transform.offsetMax = (safeArea.max - resolution) / scale;
-  }
-}
 }
